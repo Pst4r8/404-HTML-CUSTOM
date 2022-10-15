44c7d3e552abbe8312fb506374339dafff8da144
Pst4r8 <94172938+Pst4r8@users.noreply.github.com>
2022-10-15 10:18:16 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" >
+	<head>
+		<title>I Am Pst4r8</title>
+		<meta name="description" content="This is just an illusion!" />
+		<meta name="keywords" content="Pst4r8" />
+		<meta name="author" content="Pst4r8" />
+		<meta property="og:image" content="https://i.ibb.co/WcY6x5D/1664967647102.jpg"/>
+		<link rel="icon" type="image/jpg" href="https://i.ibb.co/WcY6x5D/1664967647102.jpg"/>
+		<link rel="apple-touch-icon" href="https://i.ibb.co/WcY6x5D/1664967647102.jpg" />
+	</head>
+	<body>
+		<div class="noise"></div>
+		<div class="overlay"></div>
+		<div class="terminal">
+			<h1>Error <span class="errorcode">404</span></h1>
+			<p class="output">The page you are looking for might have been removed, had its name changed or is temporarily unavailable.</p>
+			<p class="output">Please try to <a href="#1">go back</a> or <a href="#2">return to the homepage</a>.</p>
+			<p class="output">Good luck.</p>
+		</div>
+	</body>
+</html>
